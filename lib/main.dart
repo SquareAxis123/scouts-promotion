3bfa7c2171c129ec289b0c0f9f47d3c2f47c1aeb
Ryan <r2d2murphy@gmail.com>
2020-05-20 20:03:53 +0100

    added photoes

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,58 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('scouts summercamp 2019, canal boating'),
+        flexibleSpace: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'scouts summer camp 2019,',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              Text(
+                'canal boating',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Image.asset('images/logo.jfif'),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Image.asset('images/canalboating.png'),
+              ),
+              Expanded(
+                child: Image.asset('images/canalboating1.png'),
+              ),
+
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Image.asset('images/canalboating2.png'),
+              ),
+              Expanded(
+                child: Image.asset('images/canalboating3.png'),
+              ),
+
+            ],
+          ),
+        ],
       ),
     );
   }
 }
-
-
-
